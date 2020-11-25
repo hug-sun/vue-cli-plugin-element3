bd128b79d73f119d7c981fd710fa24ce64d116c2
josephxia <15906475@qq.com>
2020-11-25 15:06:00 +0800

    fix: generate opts.partial miss

--- generator/index.js
+++ generator/index.js
@@ -1,7 +1,7 @@
 module.exports = (api, opts, rootOptions) => {
   // 暂时不支持多语言
   opts.lang = 'en'
-  partial  // opts.import = 'full' // full/partial
+  opts.partial  // opts.import = 'full' // full/partial
   // opts.customTheme = false // true /false
 
   const utils = require('./utils')(api)
